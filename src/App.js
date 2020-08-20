20bba3c08880d00a62f75fa54c5c1a051efcae63
Adam Kara <kara.p.adam@gmail.com>
2020-08-20 20:33:03 +0200

    Implemented timer functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App () {
+  const [ticking, setTicking] = useState(false)
+  const [elapsedTime, setElapsedTime] = useState(0)
+
+  useEffect(() => {
+    if (ticking) {
+      const id = setInterval(() => tick(), 1000)
+      return () => {
+        clearInterval(id)
+      }
+    }
+  }, [ticking])
+
+  const tick = () => {
+    setElapsedTime(elapsedTime => elapsedTime + 1)
+  }
+
+  const start = () => {
+    setTicking(true)
+  }
+
+  const stop = () => {
+    setTicking(false)
+  }
+
+  const startStop = () => {
+    if (ticking) {
+      stop()
+    } else {
+      start()
+    }
+  }
+
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <h1>{elapsedTime}</h1>
+      <button onClick={startStop}>{ticking ? 'Pause' : 'Play'}</button>
     </div>
   )
 }
